cff0d2675fa0301667a5f19800b7e943f4f35134
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-08-02 11:39:26 +0100

    Initial commit

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -4,14 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMXy8cfauXHzfhHIp3bDAOP-nFLM6xrHs",
-  authDomain: "thel-ed136.firebaseapp.com",
-  databaseURL: "https://thel-ed136-default-rtdb.firebaseio.com",
-  projectId: "thel-ed136",
-  storageBucket: "thel-ed136.appspot.com",
-  messagingSenderId: "18007041500",
-  appId: "1:18007041500:web:182cb1eb7e039968165b19",
-  measurementId: "G-BYHJQDVC2Y"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
